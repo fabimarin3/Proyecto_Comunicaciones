c0e5b3465e208613c607c87cf6b559e67cf9f8ef
fabimarin3 <fabimr3596@gmail.com>
2018-11-17 12:41:56 -0600

    Problemas nuevos

--- Problemas/Problem_6_07.m
+++ Problemas/Problem_6_07.m
@@ -1,8 +1,8 @@
 % MATLAB script for Illustrative Problem 6, Chapter 6.
 clear
 echo on
-N=10000;					
-noise_var=1;
+N=52;					
+noise_var=0.25;
 sigma=sqrt(noise_var);			% standard deviation of the noise
 for i=1:N,				% generate data sequence
    if (rand<0.5), 
@@ -25,11 +25,25 @@ end;
 echo on ;
 y1=rec_sig1+noise;	   		% received signal with noise for channel 1
 
+
+figure(1)
+plot()
+
+
+
+
+
+
+
+% % for channel 2:
+% A=1;
+% B=[-0.2 0.5 1 0.5 -0.2];
+% rec_sig2=filter(B,A,I);    		% the received signal without noise for channel 2
+% rec_sig2=rec_sig2(3:N);	   		% to compensate for the delay in filtering 
+% for i=1:N-2,
+%    noise(i)=gngauss(sigma);
+%    echo off ;
+% end;
+% echo on ;
+% y2=rec_sig2+noise;         		% received signal with noise for channel 2
 % plotting commands follow
-figure
-plot(y1,0,'-o')
-title('Varianza "1"')
-
-%plot(rec_sig1(I),0,'x')
-%hold on
-
