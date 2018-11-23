9143cd04de6f26b2a9b1344f3b324a5accbafcdc
Jdiego Delgado <jddelgado95@gmail.com>
2018-11-22 22:24:53 -0600

    Create Problem_6_10.m
    Problema_6_10

---
+++ Problemas/Problem_6_10.m
@@ -0,0 +1,71 @@
+% MATLAB script for Illustrative Problem 6.7.
+clear
+echo on
+N=31;
+T=1;
+alpha=1/2; %Roll off factor
+n=-(N-1)/2:(N-1)/2;			% the indices for g_T
+% The expression for g_T is obtained next.
+for i=1:length(n),
+  g_T(i)=0;
+  for m=-(N-1)/2:(N-1)/2,
+    g_T(i)=g_T(i)+sqrt(xrc(4*m/(N*T),alpha,T))*exp(j*2*pi*m*n(i)/N); %Funcion del filtro 
+    echo off ;
+  end;
+end;
+echo on ;
+
+
+%%Gráfica y evaluación de gT(n) para los valores arbitrarios de alpha y N.
+
+
+ g_T=real(g_T) ; % The imaginary part is due to the finite machine precision
+ 
+n2=0:N-1;
+% impulse response of the cascade of the transmitter and the receiver filters
+g_R=g_T;
+imp_resp_of_cascade=fft(g_R);
+y=abs(imp_resp_of_cascade);
+figure (1);
+stem(fftshift(y));
+
+figure(2)
+
+x = ifft(y);
+stem(fftshift(x));
+ 
+%%Gráfica para la magnitud de la respuesta en frecuencia del filtro,
+%%realizando la transformada discreta de fourier. 
+
+% YfreqDomain = fft(g_T); %take the fft of our step funcion, y(t)
+% y=abs(YfreqDomain);
+% stem(fftshift(y));
+% xlabel('fT')
+% %ylabel('Sv/f)')
+% title('Problema 6.10')
+ 
+%  figure(2)
+%  YfreqDomain = fft(g_T); %take the fft of our step funcion, y(t)
+%  y=abs(YfreqDomain);
+%  plot(fftshift(g_T));
+%  xlabel('fT')
+%  ylabel('Sv/f)')
+%  title('Problema 6.1')
+ 
+% % Derive g_T(n-(N-1)/2).
+% n2=0:N-1;
+% % Get the frequency response characteristics.
+% [G_T,W]=freqz(g_T,1);
+% % normalized magnitude response
+% magG_T_in_dB=20*log10(abs(G_T)/max(abs(G_T)));  	
+% % %impulse response of the cascade of the transmitter and the receiver filters
+% g_R=g_T;
+% imp_resp_of_cascade=conv(g_R,g_T);
+
+% Plotting commands follow.
+
+%plot(W/(2*pi),magG_T_in_dB); %Para la respuesta en freq
+
+%stem(g_T); %Resp al impulso del filtro 
+
+%stem(imp_resp_of_cascade); %Resp al impulso del cascada
